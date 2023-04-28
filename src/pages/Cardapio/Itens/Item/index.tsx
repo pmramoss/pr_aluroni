83278914f1c0f8d8039ab2d7744510fbc79624d3
Pedro Lucas Ramos <pedromcramos13@gmail.com>
2023-04-28 15:32:46 -0300

    finalizando

---
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -0,0 +1,23 @@
+import styles from './Item.module.scss';
+import DetalhesDoPrato from 'components/DetalhesDoPrato';
+import { Prato } from 'types/Prato';
+import { useNavigate } from 'react-router-dom';
+
+export default function Item(props: Prato) {
+    const { id, title, photo, description } = props;
+    const navigate = useNavigate();
+    return (
+        <div className={styles.item} onClick={() => navigate(`/cardapio/${id}`)}>
+            <div className={styles.item__imagem}>
+                <img src={photo} alt={title} />
+            </div>
+            <div className={styles.item__descricao}>
+                <div className={styles.item__titulo}>
+                    <h2> {title} </h2>
+                    <p> {description} </p>
+                </div>
+                <DetalhesDoPrato {...props} />
+            </div>
+        </div>
+    );
+}
